4493de6c00800a3339202c0c02e4f80b0b8f27b2
duongdz2406 <100218812+duongdz2406@users.noreply.github.com>
2023-03-29 20:49:26 +0700

    update

---
+++ demoMongo.js
@@ -0,0 +1,96 @@
+const express = require('express')
+
+//import { engine } from 'express-handlebars';
+const expressHbs = require('express-handlebars');
+
+const app = express()
+
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://duongddtph24297:duongto12@cluster0.teophxq.mongodb.net/?retryWrites=true&w=majority';
+
+const labModel = require('./labModel.js');
+
+app.get('/lab', async (req, res) => {
+  await mongoose.connect(uri).then(console.log('Ket noi DB thanh cong!'));
+
+  try {
+    const labs = await labModel.find({tailieu: 2});
+
+    // labModel.updateMany();
+    // labModel.updateOne({ten: 'Lab 3'}, {ten: 'Lab 3 - 2023'})
+    // labModel.deleteMany({ten: 'Lab 4'});
+    // labModel.deleteOne({ten: 'Lab 4'});
+
+
+    console.log(labs.toString());
+    res.send(labs);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/add_lab', async (req, res) => {
+  await mongoose.connect(uri).then(console.log('Ket noi DB thanh cong!'));
+
+  let lab = new labModel ({
+    tieude : 'lab 7',
+    url: 'linktailieu.com',
+    tailieu: 2
+  });
+
+  lab.tailieu = 2;
+
+
+  try {
+    let kq = await lab.save();
+
+    console.log(kq);
+
+    let labs = await labModel.find();
+    res.send(labs);
+
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//app.engine('.hbs', ExpressHandlebars());
+app.engine('.hbs', expressHbs.engine({ 
+  extname: "hbs", 
+  defaultLayout: 'main', 
+  layoutsDir: "views/layouts/" }));
+
+//app.engine( "hbs", engine({ extname: "hbs", defaultLayout: false, layoutsDir: "views/layouts/", }) );
+
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    layout: 'main',
+    //showContentMaytinh: false,
+
+    helpers: {
+      foo() { return 'foo. CP17305 - server Android'; }
+    }
+  });
+});
+
+app.get('/maytinh', (req, res) => {
+  res.render('emptyView', {
+    layout: 'main', 
+    showContentMaytinh: true,
+    soA: 15,
+    soB: 7,
+    phepTinh: 'cong',
+    kq: 22,
+
+  });
+});
+
+
+const port = 8000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
